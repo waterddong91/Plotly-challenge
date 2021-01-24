5163a343e8c0a4fb85b2ccffc88dea2ae2a2776e
DongYi Kim <waterddong91@gmail.com>
2021-01-23 19:23:50 -0500

    include metadataTable

--- static/js/app.js
+++ static/js/app.js
@@ -1,21 +1,9 @@
-// Variables 
-var data;
-var dataNames;
-var sampledata;
-var datametadata;
-var firstID;
-var firstIDlabels;
-var selectedID;
-
-
-// ID Values from DataSamples
+// Set Values from DataSamples
 d3.json("static/js/samples.json").then(function(data){
     var data = data;
     var names = data.names;
     var samples = data.samples;
     var metadata = data.metadata;
-    //var firstID = sampledata[0].otu_ids
-    //var firstIDlabels = sampledata[0].otu_labels;
     names.forEach(function(sample) {
     var dropdownMenu = d3.select("#selDataset");
     var newoption = dropdownMenu.append("option");
@@ -35,17 +23,43 @@ d3.json("static/js/samples.json").then(function(data){
     var samples = data.samples;
     var metadata = data.metadata;
     var firstID = samples[0];
-    barchart(firstID);
-    //bubblechart(firstID);
-   // metadata(firstID);    
+    optionChanged(firstID);
 });
 
 // Set the function for changed options
 function optionChanged(sample) {
     barchart(sample);
-    //bubblechart(sample);
-    //metadata(sample);
+    bubblechart(sample);
+    metadataTable(sample);
 };
+
+
+// MetaData Table from SelectedID
+function metadataTable(sample) {
+        
+    d3.json("static/js/samples.json").then(function(data){
+        var data = data;
+        var names = data.names;
+        var metadata = data.metadata;
+        names.forEach(function(sample) {
+            var dropdownMenu = d3.select("#selDataset");
+            var newoption = dropdownMenu.append("option");
+            newoption.attr('value', sample);
+            newoption.text(sample);   
+            var selectedID = dropdownMenu.node().value;
+
+        var information = metadata.filter(samples => samples.id == selectedID)[0];
+        var panel = d3.select("#sample-metadata");
+        panel.html("");
+        var demographics = Object.entries(information)
+        demographics.forEach((item) => {
+        panel.append("h6").text(item[0]+': '+item[1]);
+        });
+        });
+    });
+
+};
+
 
 // Bar Chart from SelectedID
 function barchart(sample){
@@ -55,10 +69,11 @@ function barchart(sample){
     var samples = data.samples;
     var newID = samples.filter(sample=>sample.id == newID);
     var array = newID[0];
-    var value = array.sample_values.slice(0,10).sort((a, b) => b -a);
+    var value = array.sample_values.slice(0,10).sort((a, b) => b-a);
     var otu = array.otu_ids.map(sample => "OTU" + sample);
+    
     //otu.sort((a, b) => b-a);
-    var otuLabels = array.otu_labels.slice(0,10);
+    //var otuLabels = array.otu_labels.slice(0,10);
         
     //console.log(array);
 
@@ -78,3 +93,44 @@ function barchart(sample){
 
     });
 };
+
+// Bubble Chart from SelectedID
+
+function bubblechart(sample){
+    d3.json("static/js/samples.json").then((data) => {
+    var dropdownMenu = d3.select("#selDataset");
+    var newID = dropdownMenu.node().value;
+    var samples = data.samples;
+    var newID = samples.filter(sample=>sample.id == newID);
+    var array = newID[0];
+    var value = array.sample_values;
+    var otu = array.otu_ids;
+    otuLabels = array.otu_labels
+        
+    //console.log(array);
+
+    //plotting
+    var trace = {
+        x: otu,
+        y: value,
+        text: otuLabels,
+        mode: 'markers',
+        marker: {
+            size: value,
+            color: otu,
+            colorscale:"Earth"
+        }
+    };
+
+    var data = [trace];
+
+    var layout = {
+        xaxis: {title : "OTU ID"}
+    };
+    
+
+    Plotly.newPlot('bubble', data, layout);
+
+    });
+};
+
